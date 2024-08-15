a21f619bd624839dcced9791d8a9d8eebdf09d83
Benjaaaamín <benja_e_e@hotmail.com>
2024-08-15 13:58:05 -0400

    componeteitooon table

--- src/app/admin/users/Pagination.tsx
+++ src/app/admin/users/Pagination.tsx
@@ -1,28 +1,37 @@
 import "./pagination.css"
+import { FaRegArrowAltCircleRight, FaRegArrowAltCircleLeft } from "react-icons/fa";
 
-export const Pagination = (
-    { 
-        totalPosts, 
-        postsPerPage, 
-        setCurrentPage, 
-        currentPage 
-    }) => {
+export const Pagination = ({
+    totalPosts,
+    postsPerPage,
+    setCurrentPage,
+    currentPage
+}: {
+    totalPosts: number
+    postsPerPage: number
+    setCurrentPage: React.Dispatch<React.SetStateAction<number>>
+    currentPage: number
+}) => {
+
     let pages = [];
 
-    for (let i = 1; i <= Math.ceil(totalPosts/postsPerPage); i++) {
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
         pages.push(i)
     }
 
     return (
         <div className="pagination">
+            <div onClick={() => setCurrentPage(currentPage === 1 ? currentPage : currentPage - 1)} className="arrow"><FaRegArrowAltCircleLeft /></div>
             {
                 pages.map((page, index) => {
-                    return <button 
-                        className={page  == currentPage ? 'active' : ''} 
-                        key={index} 
+                    return <button
+                        className={page == currentPage ? 'active' : ''}
+                        key={index}
                         onClick={() => setCurrentPage(page)}>{page}</button>
                 })
             }
+            <div onClick={() => setCurrentPage(currentPage == pages.length ? currentPage : currentPage + 1)}
+                className="arrow"><FaRegArrowAltCircleRight /></div>
         </div>
     )
 }
--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -14,6 +14,8 @@ import { UserForm } from "@/ui/components/forms/createNewUserForm"
 
 /* actions */
 import {
+    Category,
+    Food,
     User
 } from "@prisma/client"
 
@@ -22,17 +24,16 @@ import { getAllUser } from "@/lib/actions/user"
 /* react */
 import React, {
     useState,
-    useEffect
+    useEffect,
+    Suspense
 } from 'react'
 
 import { Pagination } from "./Pagination"
-import UsersList from "./UsersList"
+import { FaEdit, FaTrashAlt } from "react-icons/fa"
 
 export default function Page() {
 
     const [coinsData, setCoinsData] = useState<User[]>([])
-    const [currentPage, setCurrentPage] = useState(1)
-    const [postsPerPage] = useState(5)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -53,65 +54,155 @@ export default function Page() {
         fetchData();
     }, []);
 
-    const lastPostIndex = currentPage * postsPerPage // se multiplica la pagina actual por los post por pagina
-    const firstPostIndex = lastPostIndex - postsPerPage // 
-    const currentPosts = coinsData.slice(firstPostIndex, lastPostIndex)
-
-    const column = ["id", "email", "Actions"]
-
     return (
-        <div className="w-full">
+        <div className="w-full border-2 rounded-md">
             <main className="flex gap-2 justify-between p-1">
                 <SearchAdmin />
                 <div className="flex items-center">
                     <button
                         className="p-2 hover:bg-gray-200 rounded-md"
-                        onClick={() => {}}
+                        onClick={() => { }}
                     >
                         <IoReload />
                     </button>
                     <button
                         className="p-2 hover:bg-gray-200 rounded-md"
-                        onClick={() => {}}
+                        onClick={() => { }}
                     >
                         <IoIosAddCircle />
                     </button>
                 </div>
             </main>
-            <div className="overflow-x-scroll pt-1 ">
-                <table className="w-full border-collapse bg-neutral-400 p-1 ">
-
-                    <caption className="text-black text-xl text-center 
-                        font-bold uppercase p-2">
-                        Usuarios
-                    </caption>
-
-                    <thead className="capitalize bg-neutral-800 text-neutral-300 ">
-                        <tr className="">
-                            {column.map((x) => {
-                                return (
-                                    <th className="px-6 py-4 ">{x}</th>
-                                )
-                            })}
-                        </tr>
-                    </thead>
-
-                    <UsersList data={currentPosts} />
-                    <tfoot>
-                        <tr>
-                            <td colSpan={column.length}>
-                                <Pagination
-                                    totalPosts={coinsData.length}
-                                    postsPerPage={postsPerPage}
-                                    setCurrentPage={setCurrentPage}
-                                    currentPage={currentPage}
-                                />
-                            </td>
-                        </tr>
-                    </tfoot>
-
-                </table>
+            <div className="overflow-x-auto p-1 ">
+                <Table coinsData={coinsData} />
             </div>
         </div>
+    )
+}
+
+export function Table({ coinsData }: {
+    coinsData: User[] | Food[] | Category[]
+}) {
+    const column = ["id", "email", "Actions"]
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postsPerPage] = useState(5)
+
+    const lastPostIndex = currentPage * postsPerPage // se multiplica la pagina actual por los post por pagina
+    const firstPostIndex = lastPostIndex - postsPerPage // 
+    const currentPosts = coinsData.slice(firstPostIndex, lastPostIndex)
+
+    return (
+        <table className="w-full border-collapse bg-neutral-400 p-1 ">
+
+            <caption className="text-black text-xl text-center 
+            font-bold uppercase p-2">
+                Usuarios
+            </caption>
+
+            <Thead column={column} />
+
+            <Tbody
+                column={column}
+                data={currentPosts}
+            />
+
+            <Tfoot
+                column={column}
+                data={coinsData}
+                postsPerPage={postsPerPage}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+            />
+
+        </table>
+    )
+}
+
+export function Thead({ column }: { column: String[] }) {
+    return (
+        <thead className="capitalize bg-neutral-800 text-neutral-300 ">
+            <tr className="">
+                {column.map((x) => {
+                    return (
+                        <th className="px-6 py-4 ">{x}</th>
+                    )
+                })}
+            </tr>
+        </thead>
+    )
+}
+
+export function Tbody({
+    column,
+    data
+}: {
+    column: string[]
+    data: User[] | Food[] | Category[]
+}) {
+    /* column =  ["id", "email", "Actions"] */
+    return (
+        
+        <tbody>
+            {data.map((date) => (
+                <tr className="par hover:bg-yellow-100 cursor-pointer" key={date.id}>
+                    <td className="px-6 py-4  text-center whitespace-nowrap text-sm ">{date.id}</td>
+                    <td className="px-6 py-4  text-left whitespace-nowrap text-sm ">{date.email}</td>
+                    <Actions />
+                </tr>
+            ))}
+        </tbody>
+    )
+}
+
+export function Actions() {
+    return (
+        <td className="px-6 py-4  text-center whitespace-nowrap text-sm flex items-center justify-center">
+            <button
+                onClick={() => { }}
+                className="text-blue-600 hover:text-blue-900 mr-2 p-2 hover:bg-gray-200 rounded-md"
+            >
+                <FaEdit />
+            </button>
+            <button
+                onClick={() => { }}
+                className="text-red-600 hover:text-red-900 p-2 hover:bg-gray-200 rounded-md"
+            >
+                <FaTrashAlt />
+            </button>
+        </td>
+    )
+}
+export function Tfoot({
+    column,
+    data,
+    postsPerPage,
+    setCurrentPage,
+    currentPage
+}: {
+    column: String[]
+    data: User[] | Food[] | Category[]
+    postsPerPage: number
+    setCurrentPage: React.Dispatch<React.SetStateAction<number>>
+    currentPage: number
+}) {
+
+    const totalPost = data.length
+    console.log(totalPost)
+    console.log(currentPage)
+
+    return (
+        <tfoot>
+            <tr>
+                <td colSpan={column.length}>
+                    <Pagination
+                        totalPosts={totalPost}
+                        postsPerPage={postsPerPage}
+                        setCurrentPage={setCurrentPage}
+                        currentPage={currentPage}
+                    />
+                </td>
+            </tr>
+        </tfoot>
     )
 }
